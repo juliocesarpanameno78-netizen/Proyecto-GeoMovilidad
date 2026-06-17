da1cb7e9ba6886d3b1e21d353c4a94f7e0c53f5b
Julio Panameño <juliocesarpanameno78@gmail.com>
2026-06-17 16:06:29 -0500

    Se arreglo la ruta de solicitar una demarcacion, se añadio la ruta de hacer una PQRFS

---
+++ controller/Demarcaciones/DemarcacionesController.php
@@ -0,0 +1,11 @@
+<?php
+
+    include_once '../model/Demarcaciones/DemarcacionesModel.php';
+
+    class DemarcacionesController{
+        public function getCreate(){
+            include_once '../view/Demarcaciones/create.php';
+        }
+    
+    }
+?>
